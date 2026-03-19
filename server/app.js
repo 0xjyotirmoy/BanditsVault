57200da265a53f50cbc68bdfbfd05059a8463a77
Jyotirmoy Karmakar <jyotirmoy3155@gmail.com>
2026-03-19 23:08:09 +0530

    fix(vercel): restructure to api/ and public/ for zero-config deployment

--- server/app.js
+++ server/app.js
@@ -19,21 +19,21 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/api/encrypt', createLimiter, encryptRoute);
 app.use('/api/vault', vaultRoute);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/create', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/create.html'));
+  res.sendFile(path.join(__dirname, '../public/create.html'));
 });
 
 app.get('/vault/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/vault.html'));
+  res.sendFile(path.join(__dirname, '../public/vault.html'));
 });
 
 if (require.main === module) {
